8be5e5ce201fc517f9332012ce2a7064736533a5
wan00000 <163032054+wan00000@users.noreply.github.com>
2025-01-04 13:29:01 +0800

    revise

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,17 @@
+import { Stack } from 'expo-router';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+const Layout = () => {
+    return (
+        <Stack>
+            <Stack.Screen name="welcome" options={{ headerShown: false }} />
+            <Stack.Screen name="sign-up" options={{ headerShown: false }} />
+            <Stack.Screen name="sign-in" options={{ headerShown: false }} />
+        </Stack>
+    );
+}
+
+
+
+export default Layout;
